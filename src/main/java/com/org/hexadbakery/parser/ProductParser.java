d7f33c2eebf3cac00111fe1138652f44e184c07a
Ravi <ravichandra.p@outlook.com>
2019-05-23 16:06:48 +0200

    Create ProductParser.java

---
+++ src/main/java/com/org/hexadbakery/parser/ProductParser.java
@@ -0,0 +1,20 @@
+package com.org.hexadbakery.parser;
+
+import static java.util.stream.Collectors.toMap;
+
+import java.util.List;
+import java.util.Map;
+
+import com.org.hexadbakery.models.Product;
+
+public class ProductParser implements Parser<Product> {
+	@Override
+	public Map<String, Product> parse(List<String> lines) {
+		return lines.stream().map(this::getProduct).collect(toMap(Product::getCode, product -> product));
+	}
+
+	private Product getProduct(String row) {
+		String[] values = row.split(CSV_SEPARATOR);
+		return new Product(values[0].trim(), values[1].trim());
+	}
+}
